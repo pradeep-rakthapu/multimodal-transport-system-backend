63c569b66bf99a5ec26bb0785dea76df07d8cc6b
pradeep <pradeepgoud.rpg@gmail.com>
2025-08-13 21:16:08 +0530

    redis-fix

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,5 +1,17 @@
-import { createClient } from 'redis';
-const redisClient = createClient();
-redisClient.on('error', err => console.error('Redis Error:', err));
+import { createClient } from "redis";
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+  socket: process.env.REDIS_TLS === "true" ? {
+    tls: true,
+    rejectUnauthorized: false
+  } : {}
+});
+
+redisClient.on("error", (err) => {
+  console.error("❌ Redis Error:", err);
+});
+
 await redisClient.connect();
+
 export default redisClient;
